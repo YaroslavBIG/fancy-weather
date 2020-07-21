b6fa541165fc5d901a3f04a84b2839664c8530d2
FireGun <50775604+YaroslavBIG@users.noreply.github.com>
2020-07-22 02:32:44 +0300

    fix: delete api keys

--- src/js/api/_keys.js
+++ src/js/api/_keys.js
@@ -1,19 +1,19 @@
 // Location
-const openCageData = '8396a22a3ed045debd14e2953aa58a40';
+const openCageData = '000000000000000000000000000000'
 // // IpInfo ex: https://ipinfo.io/?token=05bc39921f4dfb
-const ipInfo = '05bc39921f4dfb';
+const ipInfo = '00000000000000
 // // Weather
-const openWeatherMap = '2c01ff384e962fecf244180e50c71d33';
-const weatherBit = 'fac30ed1759e4f48bedbd8bbbf3ec09a';
+const openWeatherMap = '00000000000000000000'
+const weatherBit = '000000000000000000000000000000'
 // // Clima cell https://developer.climacell.co/v3/docs/tile-integrations
-const climaCell = 'TGqNjAaQ2jOheaVUWf8Vyt4dQGwCAg3Z';
+const climaCell = '000000000000000000000000000000'
 // // MapBox
-const mapBox = 'pk.eyJ1IjoiZmlyZWd1biIsImEiOiJja2FtbzZkY3QwOHE4MnlwZnZweXZobXJ1In0.xpIZUEZdbfR30a9zb7ehlA';
+const mapBox = '000000000000000000000000000000'
 // // Image
-const unsplash = 'ovcpQNeL8ujHlMf-bYnUm6dr-V33nLpW43SiXFHoF-g';
-const flickr = '5109930d3cd7f4f540b96a8c35cfd64e';
+const unsplash = '000000000000000000000000000000'
+const flickr = '000000000000000000000000000000'
 // // Translate
-const yandexTranslateKey = 'trnsl.1.1.20200428T150322Z.a6a1bee98403ab63.106cdb7826f9e664bc0f48ef68b076b9f5e52977';
+const yandexTranslateKey = '000000000000000000000000000000'
 
 
 export {
